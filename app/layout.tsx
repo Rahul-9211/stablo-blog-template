13c509293967ea71f96b0b1a0462cc0bf4f05391
rahul-lal10 <rahul.rawat@lal10.com>
2024-10-16 14:50:58 +0530

    gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,6 +23,17 @@ export default function RootLayout({
       lang="en"
       suppressHydrationWarning
       className={cx(inter.variable, lora.variable)}>
+       <script async src="https://www.googletagmanager.com/gtag/js?id=G-CT8789G4S4"></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-CT8789G4S4');
+          `,
+        }}
+      />
       <body className="antialiased text-gray-800 dark:bg-black dark:text-gray-400">
         <Providers>{children}</Providers>
       </body>
